f2f0b538a071303f7c1d5495429cee50dff68535
Kevin Mancilla <kevin_corsman@hotmail.com>
2020-12-11 00:41:35 -0600

    Primera parte del funcionamiento de los checkpoints
    Se programaron todas las interacciones con el jugador, asi como eliminacion y adicion de lineas de codigo del jugador

--- To The Space/Assets/Scripts/Spacecraft.cs
+++ To The Space/Assets/Scripts/Spacecraft.cs
@@ -5,20 +5,18 @@ using UnityEngine;
 public class Spacecraft : SpaceShips
 {
     public float speed;                                                 //Flotante para controlar la velocidad
-    public float gravity;                                               //flotante para controlar la gravedad
-    
+    public float gravity;                                               //Flotante paara aplicar gravedad al momento de volar
+    float currentGravity;                                               //Flotante para controlar la gravedad actual
+    float tempGravity = 0;                                              //Flotante para convertir la gravedad 0
+    Vector2 currentCheckpoint;
     void Start()
     {
-        
+        currentGravity = gravity;
     }
     
     void Update()
     {
-        if (transform.position.y > -3.83f)                              //Si superamos -3.83 en el eje y, significa que estamos en el aire
-        {
-            IsFlying();                                                 //Llamamos al metodo
-        }
-        transform.Translate(Vector3.down * gravity * Time.deltaTime);   //Con esta linea aplicamos gravedad
+        transform.Translate(Vector3.down * currentGravity * Time.deltaTime);   //Con esta linea aplicamos gravedad
         if (Input.GetKey(KeyCode.W))                                    //Si se presiona W la nave se mueve hacia arriba
         {
             transform.Translate(Vector3.up * speed * Time.deltaTime);
@@ -38,20 +36,51 @@ public class Spacecraft : SpaceShips
     {
         if (collision.tag == "Checkpoint")                              //Si colicionamos con un checkpoint
         {
+            currentCheckpoint = collision.transform.position;
+            Debug.Log("Llegue al primer checkpoint");
             //texto que indica que se ha alcanzado un nuevo checkpoint
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "FirstPoint")
+        {
+            currentCheckpoint = collision.transform.position;
+            Debug.Log("Sali del primer punto");
+            isFlying = true;
+            Fly();
         }
     }
 
     protected void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Checkpoint")                       //Si colicionamos con un checkpoint
+        if (collision.gameObject.tag == "Platform")                       //Si colicionamos con la plataforma
         {
+            Debug.Log("Estoy en la primer plataforma");
+            currentGravity = tempGravity;
             canRefuel = true;
-            IsRefueling();
+            isFlying = false;
+            if (Input.GetKey(KeyCode.R))                                  //Si se presiona W la nave se mueve hacia arriba
+            {
+                Debug.Log("Recargando combustible");
+                IsRefueling();
+            }
         }
         if(collision.gameObject.tag == "Enemy")
         {
             IsDead();
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            canRefuel = false;
+            currentGravity = gravity;
+
+            isFlying = true;
+            Fly();                                                       //Llamamos al metodo
         }
     }
 }
