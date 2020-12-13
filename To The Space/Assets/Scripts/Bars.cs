196e0b2e27318768e32ec7bb479e2c43f30294f3
Kevin Mancilla <kevin_corsman@hotmail.com>
2020-12-13 00:38:52 -0600

    Tercera parte del funcionamiento de los checkpoints e interaccion con barras de vida,combustible,etc
    se estuvo trabajando en los checkpoints, la interaccion entre estos y las barras, asi como mejorando el aspecto visual de estas ultimas mediante codigo y sprites

--- To The Space/Assets/Scripts/Bars.cs
+++ To The Space/Assets/Scripts/Bars.cs
@@ -1,34 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bars : MonoBehaviour
 {
-    [SerializeField] private BarsScriptableObject barFuel;
 
+    public Slider slider;//Variable para controlar el slider
+    public Gradient gradient;//Variable para controlar el gradient
+    public Image fill;//Variable para poder emplear los "efectos" del gradietn ala imagen
+    public Image icon;
+    public float transparency;
 
-    public RectTransform recTransform;                                              //Este rectranform nos ayudara a interactuar con la barra de combustible
-    public float capacity;                                                       //La declaramos estatica para que podamos modificarla desde cualquier otro script
-    public float maxCapacity;                                                    //Variable que nos permitira controlar la capacidad maximacalled before the first frame update
+    public enum Mode { Neutral, Show, Hide };
+    public Mode mode;
 
-    void Start()
+    private void Start()
     {
-        capacity = barFuel.capacity;
-        maxCapacity = barFuel.maxCapacity;
-        recTransform = GetComponent<RectTransform>();
+        mode = Mode.Neutral;
+
+        icon.canvasRenderer.SetAlpha(1.0f);
     }
-
-
-    void Update()
+    private void Update()
     {
-        float updateBar = capacity;   //Creamos una variable temporal. moverse hacia cierto numero a una velocidad especifica 
-        recTransform.sizeDelta = new Vector2(14.5f, Mathf.Clamp(updateBar, 0.0f, maxCapacity));//(valor en X, modificamos valor en Y con un limite negativo y uno positivo
-        if(capacity < 1)
+        if (slider.value < 50 && mode == Mode.Neutral)
         {
-            capacity = 0;
-            //recTransform.sizeDelta = new Vector2(14.5f,0);//(valor en X, modificamos valor en Y con un limite negativo y uno positivo
+            mode = Mode.Hide;
+        }
+        if(slider.value > 50)
+        {
+            mode = Mode.Neutral;
+            if (transparency >= 1)
+            {
+                transparency = 1;
+            }
+            if (transparency < 1f)
+            {
+                transparency += Time.deltaTime;
+                icon.color = new Color(icon.color.r, icon.color.r, icon.color.b, transparency);
+            }
+        }
 
+        if(mode == Mode.Hide)
+        {
+
+
+            if (transparency <= 0.01f)
+            {
+                transparency = 0.01f;
+                mode = Mode.Show;
+            }
+            if (transparency > 0.01f)
+            {
+                transparency -= Time.deltaTime;
+                icon.color = new Color(icon.color.r, icon.color.r, icon.color.b, transparency);
+            }
+        }
+
+        if(mode == Mode.Show)
+        {
+            if (transparency >= 1)
+            {
+                transparency = 1;
+
+                mode = Mode.Hide;
+            }
+            if(transparency < 1f)
+            {
+                transparency += Time.deltaTime;
+                icon.color = new Color(icon.color.r, icon.color.r, icon.color.b, transparency);
+            }
+        }
+
+        if (slider.value < 1)
+        {
+            slider.value = 0;
         }
     }
+    /// <summary>
+    /// Metodo que nos permite controlar la cantidad maxima
+    /// </summary>
+    /// <param name="quantity"></param>
+    public void SetMaxQuantity(float quantity)
+    {
+        slider.maxValue = quantity;//Le pasamos la cantidad maxima
+        slider.value = quantity;//Le pasamos el valor actual a value
 
+        fill.color = gradient.Evaluate(1f);//obtenemos un color de nuestro gradient en un punto especifico
+
+    }
+
+    public void SetQuantity(float quantity)
+    {
+        slider.value = quantity;
+        fill.color = gradient.Evaluate(slider.normalizedValue);//Como nuestra cantidad mazima puede variar y el gradient solo funcion de 0-1 , al normalizarlo "cambiamos" los valores a 0-1
+    }
 }
